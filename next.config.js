070414bc9be37acebcf4d612e1992d351416ecb7
Raellopes368 <raellopes368@gmail.com>
2021-12-27 17:39:17 -0300

    update: sass config next

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
-const withImages = require('next-images');
+const path = require('path');
 
-module.exports = withImages({
-  esModule: true,
-});
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+
+}
