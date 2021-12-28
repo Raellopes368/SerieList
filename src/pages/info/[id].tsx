cb6918fcdec99640b5d85469753508e8236613b7
Raellopes368 <raellopes368@gmail.com>
2021-12-28 17:55:27 -0300

    add: search informations with TMDB API

--- src/pages/info/[id].tsx
+++ src/pages/info/[id].tsx
@@ -1,11 +1,62 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import { FiArrowLeft } from 'react-icons/fi';
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
 import Modal from '../../components/Modal';
 import Button from '../../components/Button';
 import styles from './Info.module.scss';
+import api from '../../services/api';
 
-export default function Info() {
+export type ApiResponse = {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  name: string;
+  overview: string;
+  number_of_episodes: number;
+  number_of_seasons: number;
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { params } = context;
+  if (params?.id) {
+    const { id } = params;
+    try {
+      const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+      const { data } = await api.get<ApiResponse>(
+        `https://api.themoviedb.org/3/tv/${id}?api_key=${apiKey}&language=pt-BR&page=1`
+      );
+
+      const baseUrlImage = 'https://image.tmdb.org/t/p/w500';
+
+      return {
+        props: {
+          serieInfo: {
+            ...data,
+            image: data.backdrop_path
+              ? `${baseUrlImage}${data.backdrop_path}`
+              : `${baseUrlImage}${data.poster_path}`,
+            description: data.overview,
+            episodes: data.number_of_episodes,
+            seasons: data.number_of_seasons,
+          },
+        },
+      };
+    } catch (error) {
+      //
+    }
+  }
+
+  return {
+    props: {
+      serieInfo: {},
+    },
+  };
+};
+
+export default function Info({
+  serieInfo,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   const [showModal, setShowModal] = useState(false);
   const router = useRouter();
   return (
@@ -17,35 +68,38 @@ export default function Info() {
         <div className={styles.imageContainer}>
           <img
             className={styles.imgHeader}
-            src="https://i2.wp.com/3.bp.blogspot.com/-N7kMwSGyAVI/WWeZXMU1qjI/AAAAAAAANnk/HYZ3m3uRG4YhW4pu8iOOQtvbDh1BzymQQCLcBGAs/s1600/Vikings.jpg?ssl=1"
-            alt=""
+            src={serieInfo.image}
+            alt={serieInfo.name}
           />
         </div>
         <img
           className={styles.img}
-          src="https://i2.wp.com/3.bp.blogspot.com/-N7kMwSGyAVI/WWeZXMU1qjI/AAAAAAAANnk/HYZ3m3uRG4YhW4pu8iOOQtvbDh1BzymQQCLcBGAs/s1600/Vikings.jpg?ssl=1"
-          alt=""
+          src={serieInfo.image}
+          alt={serieInfo.name}
         />
       </header>
       <div className={styles.content}>
-        <h1 className={styles.title}>Vikings</h1>
-        <span className={styles.seasons}>Temporadas: 8</span>
+        <h1 className={styles.title}>{serieInfo.name}</h1>
+        <span className={styles.seasons}>Temporadas: {serieInfo.seasons}</span>
+        <span className={styles.seasons}>Episódios: {serieInfo.episodes}</span>
         <span className={styles.seasons}>Status: Assistido</span>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industrys standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          rediving essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </p>
+        <p>{serieInfo.description}</p>
 
-        <Button onClick={() => setShowModal(true)} title="Salvar na minha lista" />
+        <Button
+          onClick={() => setShowModal(true)}
+          title="Salvar na minha lista"
+        />
       </div>
-      { showModal && <Modal id={0} title='Vikings' close={() => setShowModal(false)}/>}
+      {showModal && (
+        <Modal id={0} title="Vikings" close={() => setShowModal(false)} />
+      )}
     </main>
   );
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [`/info/[id]`],
+    fallback: true,
+  };
 }
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,10 +1,67 @@
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
-import React from 'react';
 import { FiSearch, FiArrowLeft } from 'react-icons/fi';
 import ItemSearched from '../../components/ItemSearched';
 import styles from './Search.module.scss';
+import api from '../../services/api';
 
+let time: NodeJS.Timeout | null = null;
+
+export type SerieResponse = {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  name: string;
+  overview: string;
+};
+
+export type SerieInformation = {
+  id: number;
+  image: string;
+  name: string;
+  description: string;
+};
+
+type TApiResponse = {
+  results: SerieResponse[];
+};
 export default function Search() {
+  const [serieName, setSerieName] = useState('');
+  const [serieList, setSerieList] = useState<SerieInformation[]>([]);
+
+  async function searchSerieByName() {
+    try {
+      if (serieName.trim().length) {
+        const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+        const { data } = await api.get<TApiResponse>(
+          `/search/tv?api_key=${apiKey}&language=pt-BR&page=1&query=${serieName}`
+        );
+
+        const baseUrlImage = 'https://image.tmdb.org/t/p/w500';
+
+        const allSeriesFormateds = data.results
+          .map((result) => ({
+            id: result.id,
+            name: result.name,
+            description: result.overview,
+            image: result.backdrop_path
+              ? `${baseUrlImage}${result.backdrop_path}`
+              : `${baseUrlImage}${result.poster_path}`,
+          }))
+          .filter((serie) => serie.description);
+
+        setSerieList(allSeriesFormateds);
+      } else setSerieList([]);
+    } catch (error) {
+      //
+    }
+  }
+
+  useEffect(() => {
+    if (time) clearTimeout(time);
+    time = setTimeout(searchSerieByName, 1000);
+  }, [serieName]);
+
   return (
     <main className={styles.container}>
       <header className={styles.header}>
@@ -12,20 +69,25 @@ export default function Search() {
           <FiArrowLeft className={styles.goBack} color="#C70039" size={45} />
         </Link>
         <div className={styles.searchContainer}>
-          <input type="text" className={styles.searchInput} />
+          <input
+            type="text"
+            placeholder="Nome da série"
+            className={styles.searchInput}
+            value={serieName}
+            onChange={(e) => setSerieName(e.target.value)}
+          />
           <FiSearch color="#C70039" size={35} />
         </div>
       </header>
       <main className={styles.content}>
-        <ItemSearched />
-        <ItemSearched />
-        <ItemSearched />
-        <ItemSearched />
-        <ItemSearched />
-        <ItemSearched />
-        <ItemSearched />
-        <ItemSearched />
-        <ItemSearched />
+        {serieList.map((serie) => (
+          <ItemSearched key={serie.id} {...serie} />
+        ))}
+        {!serieList.length && (
+          <div className={styles.empty}>
+            <span>Busque suas séries</span>
+          </div>
+        )}
       </main>
     </main>
   );
