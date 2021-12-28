ed473341041112d6d8f1811edff35b507df22618
Raellopes368 <raellopes368@gmail.com>
2021-12-28 17:49:15 -0300

    add: get popular series with TMDB API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,45 @@
 import React from 'react';
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
 import Head from 'next/head';
 import styles from '../../styles/Home.module.css';
 import Header from '../components/Header';
 import Main from '../components/Main';
 import StoryList from '../components/StoryList';
+import api from '../services/api';
 
-export default function Home() {
+type TSerieStory = {
+  id: number;
+  backdrop_path: string;
+  name: string;
+};
+
+type TSerieResults = {
+  results: TSerieStory[];
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+  try {
+    const { data } = await api.get<TSerieResults>(
+      `/tv/popular?api_key=${apiKey}&language=pt-BR&page=1`
+    );
+    const series = data.results;
+    return {
+      props: {
+        series,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        series: [],
+      },
+    };
+  }
+};
+
+
+export default function Home({ series }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className={styles.container}>
       <Head>
@@ -17,10 +51,9 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <Header />
-        <StoryList />
+        <StoryList series={series}/>
         <Main />
       </main>
     </div>
   );
-};
-
+}
