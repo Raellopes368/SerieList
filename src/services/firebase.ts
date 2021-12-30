0364f3d3c569f183947c51048d983d7ea98b41a0
Raellopes368 <raellopes368@gmail.com>
2021-12-30 20:13:45 -0300

    add: add database export

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,10 +1,14 @@
 import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY_FIREBASE,
   appId: process.env.NEXT_PUBLIC_APP_ID,
   authDomain: process.env.NEXT_PUBLIC_APP_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_APP_PROJECT_ID
 };
 
+initializeApp(firebaseConfig);
 
-export default initializeApp(firebaseConfig);
+export const database = getFirestore();
