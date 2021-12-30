26f80096fbc9d96c17a13b96665faad0fa043719
Raellopes368 <raellopes368@gmail.com>
2021-12-30 19:37:22 -0300

    add: add Button to Loggout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,26 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import { useRouter } from 'next/router';
 import { FiSearch } from 'react-icons/fi';
+import { GoSignOut } from 'react-icons/go';
 import Link from 'next/link';
+import { UseAuthContext } from '../../hooks/AuthContext';
 import styles from './Header.module.scss';
 
 export default function Header() {
+  const { signOut } = UseAuthContext();
+  const router = useRouter();
+
+  function loggout() {
+    signOut();
+    router.push('/login');
+  }
+
   return (
     <div className={styles.container}>
+      <button type="button" className={styles.signOut} onClick={loggout}>
+        Sair <GoSignOut color="#fff" size={21} />
+      </button>
       <div className={styles.row}>
         <h1 className={styles.title}>Serie List</h1>
         <Link href="/search">
