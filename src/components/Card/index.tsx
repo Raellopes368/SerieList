aaafce7f0242f5476c376586c5148831fba55fde
Raellopes368 <raellopes368@gmail.com>
2022-01-01 19:57:01 -0300

    add: add serie info by props

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,21 +1,22 @@
 import Link from 'next/link';
 import React from 'react';
 import { AiOutlineInfoCircle, AiOutlineMore } from 'react-icons/ai';
+import { TSerieItem } from '../../pages';
 import styles from './Card.module.scss';
 
-export default function Card() {
+export default function Card({ name, image, serieId}: TSerieItem) {
   return (
     <div className={styles.container}>
       <div className={styles.imageContent}>
         <h1 className={styles.name}>Vikings</h1>
         <img
           className={styles.img}
-          src="https://observatoriodocinema.uol.com.br/wp-content/uploads/2020/04/Vikings.jpeg"
-          alt="Vikings"
+          src={image}
+          alt={name}
         />
       </div>
       <footer className={styles.footer}>
-        <Link href="/info/0">
+        <Link href={`/info/${serieId}`}>
           <AiOutlineInfoCircle
             className={styles.icons}
             color="#fff"
