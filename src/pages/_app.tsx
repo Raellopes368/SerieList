391cc344f14603d5c17f5abb3ae739f7919aa9bd
Raellopes368 <raellopes368@gmail.com>
2022-01-02 19:36:59 -0300

    add: SerieList Provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
 import { AuthProvider } from '../hooks/AuthContext';
-import '../../styles/globals.scss'
+import { SerieListProvider } from '../hooks/SerieListContext';
+import '../../styles/globals.scss';
 import '../services/firebase';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      <SerieListProvider>
+        <Component {...pageProps} />
+      </SerieListProvider>
     </AuthProvider>
   );
 }
 
-export default MyApp
+export default MyApp;
