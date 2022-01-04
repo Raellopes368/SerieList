7a7235c6a34ae9c1f7186f8641eae496f444c528
Raellopes368 <raellopes368@gmail.com>
2022-01-04 18:58:55 -0300

    add: add loading

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import Link from 'next/link';
 import React, { FormEvent, useState } from 'react';
+import Loaging from 'react-loading';
 import { UseAuthContext } from '../../hooks/AuthContext';
 import { checkFieldIsEmpty } from '../../utils/checkFieldIsEmpty';
 import styles from './Login.module.scss';
@@ -9,15 +10,23 @@ export default function Login() {
   const { signInWithEmail, signInWithGoogle } = UseAuthContext();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
 
   async function login(e: FormEvent) {
     e.preventDefault();
+    setError('');
+    setLoading(true);
     try {
       if (checkFieldIsEmpty(email) && checkFieldIsEmpty(password)) {
         await signInWithEmail(email, password);
+      } else {
+        setError('Informe todos os campos!');
       }
-    } catch (error) {
-      console.log(error);
+    } catch (err) {
+      setError('Usuário ou senha incorretos!');
+    } finally {
+      setLoading(false);
     }
   }
 
@@ -25,6 +34,7 @@ export default function Login() {
     <div className={styles.container}>
       <form className={styles.form} onSubmit={login}>
         <h1 className={styles.title}>Serie List</h1>
+        {!!error && <span className={styles.error}>{error}</span>}
         <input
           type="email"
           placeholder="E-mail"
@@ -35,7 +45,15 @@ export default function Login() {
           placeholder="Senha"
           onChange={(e) => setPassword(e.target.value)}
         />
-        <input type="submit" value="Entrar" />
+        <button type="submit" className={styles.loginButton} disabled={loading}>
+          {!loading ? (
+            'Entrar'
+          ) : (
+            <div className={styles.loadingContainer}>
+              <Loaging color="#fff" height="50%" width="50%" type="spin" />
+            </div>
+          )}
+        </button>
         <span>
           Ainda não possui cadastro?{' '}
           <Link href="/register">
--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import Link from 'next/link';
 import React, { FormEvent, useState } from 'react';
+import Loaging from 'react-loading';
 import { UseAuthContext } from '../../hooks/AuthContext';
 import { checkFieldIsEmpty } from '../../utils/checkFieldIsEmpty';
 import styles from './Register.module.scss';
@@ -10,28 +11,37 @@ export default function Register() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [repeatedPass, setRepeatedPass] = useState('');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
 
   async function register(e: FormEvent) {
     e.preventDefault();
+    setError('');
     if (
       checkFieldIsEmpty(email) &&
       checkFieldIsEmpty(password) &&
       checkFieldIsEmpty(repeatedPass)
     ) {
       if (password === repeatedPass) {
+        setLoading(true);
         try {
           await registerWithEmail(email, password);
-        }catch(err) {
-          console.log(err);
+        } catch (err) {
+          setError('Houve algum erro, tente novamente!');
+        } finally {
+          setLoading(false);
         }
+      } else {
+        setError('As senhas não coincidem!');
       }
-    }
+    } else setError('Informe todos os campos!');
   }
 
   return (
     <div className={styles.container}>
       <form className={styles.form} onSubmit={register}>
         <h1 className={styles.title}>Serie List</h1>
+        {!!error && <span className={styles.error}>{error}</span>}
         <input
           type="email"
           placeholder="E-mail"
@@ -50,7 +60,19 @@ export default function Register() {
           value={repeatedPass}
           onChange={(e) => setRepeatedPass(e.target.value)}
         />
-        <input type="submit" value="Cadastrar" />
+        <button
+          type="submit"
+          className={styles.registerButton}
+          disabled={loading}
+        >
+          {!loading ? (
+            'Cadastrar'
+          ) : (
+            <div className={styles.loadingContainer}>
+              <Loaging color="#fff" height="50%" width="50%" type="spin" />
+            </div>
+          )}
+        </button>
         <span>
           Já tenho conta{' '}
           <Link href="/login">
